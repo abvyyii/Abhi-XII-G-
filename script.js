18f5a7afb844119b8819c5f76cc0e7b02872fd67
Abhii <157186373+abvyyii@users.noreply.github.com>
2024-09-14 19:23:58 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+function ans1(){
+document.getElementById("ans1").style.transform="translate(0%,0%) scale(1)";
+document.getElementById("ans1").style.visibility="visible";
+}
+function close1(){
+    document.getElementById("ans1").style.transform="translate(-50%,-50%) scale(0.01)";
+    document.getElementById("ans1").style.visibility="hidden";
+    document.getElementById("ans1").style.height="auto";
+}
+function ans2(){
+    document.getElementById("ans2").style.transform="translate(0%,0%) scale(1)";
+    document.getElementById("ans2").style.visibility="visible";
+    }
+    function close2(){
+        document.getElementById("ans2").style.transform="translate(-50%,-50%) scale(0.01)";
+        document.getElementById("ans2").style.visibility="hidden";
+        document.getElementById("ans2").style.height="auto";
+    }
